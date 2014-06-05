b187edd656d9e1fd8db53e6b03364adf46f2930d
shaker <shakalghaithi@gmail.com>
2014-06-04 20:37:01 -0700

    adding the views,models and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,17 @@
 BogApp::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root to: 'bogs#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get '/bogs', to: 'bogs#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get '/bogs/new', to: 'bogs#new'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  post '/bogs', to: 'bogs#create'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  get '/bogs/:id', to: 'bogs#show'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  get '/bogs/:id/edit', to: 'bogs#edit'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  put '/bogs/:id', to: 'bogs#update'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get '/bogs/:id/delete', to: 'bogs#delete'
 end
